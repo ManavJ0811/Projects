ca7bd7838bc0478bdd07617a22c92eca6877405c
ManavJ0811 <messixgod336@gmail.com>
2025-09-29 14:50:54 +0530

    Add files via upload

---
+++ Weather program.py
@@ -0,0 +1,18 @@
+import requests
+
+api_key = '32213aaf3156e3f1b07810202afe8d7f'
+
+user_input = input("Enter city: ")
+
+# Use metric units to get temperature in Celsius
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºC")
